34d3d90c57c4832126eac9d65de500c82e35abed
Gina <sprint@gonzaga.edu>
2023-04-12 03:16:07 +0000

    repl auto format

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -10,28 +10,28 @@ using namespace std;
 // can be expanded to store multiple values
 // or templated to store any value type
 class DoublyLinkedList {
-	private:
-		// self referential
-	   struct Node {
-		  int value; // data value Node stores
-		  struct Node * next; // points to next Node in list
-      struct Node * prev; // NEW: points to previous Node in list
-	   }; 
+private:
+  // self referential
+  struct Node {
+    int value;         // data value Node stores
+    struct Node *next; // points to next Node in list
+    struct Node *prev; // NEW: points to previous Node in list
+  };
 
-	   Node * head; // list head pointer
+  Node *head; // list head pointer
 
-	public:
-	   DoublyLinkedList(); // DVC
-	   ~DoublyLinkedList(); // destructor
-		  
-	   // common linked list operations
-	   void insertAtFront(int);
-	   void displayList();
-	   void appendNode(int); // insert at end
-	   void insertInOrder(int);
-	   void deleteAtFront();
-	   void deleteAtEnd();
-	   void deleteNode(int);
+public:
+  DoublyLinkedList();  // DVC
+  ~DoublyLinkedList(); // destructor
+
+  // common linked list operations
+  void insertAtFront(int);
+  void displayList();
+  void appendNode(int); // insert at end
+  void insertInOrder(int);
+  void deleteAtFront();
+  void deleteAtEnd();
+  void deleteNode(int);
 };
 
 #endif
